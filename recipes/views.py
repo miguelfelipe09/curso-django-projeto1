caf13974bea50c3fbd3050bf47db5af2315b31fa
Miguel Felipe <miguelf1618@gmail.com>
2022-09-21 21:24:59 -0300

    Updates

---
+++ recipes/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('Home')
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
