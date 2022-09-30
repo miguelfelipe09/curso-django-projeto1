6fb2911397526d80e9b440daf3bf862d7dcf0d23
Miguel Felipe <miguelf1618@gmail.com>
2022-09-30 14:34:44 -0300

    updates

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,16 @@
 from django.http import Http404
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.recipes.factory import make_recipe
 
 from .models import Recipe
 
 
 def home(request):
+
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -27,10 +29,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id,
-        is_published=True,
-    ).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
