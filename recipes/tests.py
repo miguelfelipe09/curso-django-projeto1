bc60116d71d44e90200aa9dfa91778b9c76e6cbd
Miguel Felipe <miguelf1618@gmail.com>
2022-10-09 21:24:43 -0300

    test recipe

--- recipes/tests.py
+++ recipes/tests.py
@@ -26,3 +26,7 @@ class RecipeViewsTest(TestCase):
     def test_recipe_category_views_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_views_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
