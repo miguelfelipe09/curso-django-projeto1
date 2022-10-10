8531d135e8f5de9589310fd804c03e13e1a9082a
Miguel Felipe <miguelf1618@gmail.com>
2022-10-10 15:25:02 -0300

    test

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,9 +1,11 @@
-from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
 
+from .test_recipe_base import RecipeTestBase
 
-class RecipeViewsTest(TestCase):
+
+class RecipeViewsTest(RecipeTestBase):
+
     def test_recipe_home_views_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -23,6 +25,9 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
 
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
         response = self.client.get(reverse('recipes:home'))
